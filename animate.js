d2986832b0ba845a230066e767b6870411bc8380
Angela Lam <30443453+kyrnen@users.noreply.github.com>
2019-06-13 16:58:55 -0700

    create animate.js

---
+++ animate.js
@@ -0,0 +1,8 @@
+function showNav() {
+  document.getElementById("aboutMe").style.display = "inline";
+  document.getElementById("projects").style.display = "inline";
+  document.getElementById("hobbies").style.display = "inline";
+  document.getElementById("resume").style.display = "inline";
+}
+
+setTimeout("showNav()", 3000)
