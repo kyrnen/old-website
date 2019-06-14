9e4534fea3fe116c0951e27399820b3d6a5d3e4b
Angela Lam <30443453+kyrnen@users.noreply.github.com>
2019-06-13 19:54:09 -0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function showNav() {
-  document.getElementById("aboutMe").style.display = "inline";
-  document.getElementById("projects").style.display = "inline";
-  document.getElementById("hobbies").style.display = "inline";
-  document.getElementById("resume").style.display = "inline";
-}
-
-setTimeout("showNav()", 3000)
