8bc10f0d1866d5cd8e3fc859264ee4b81a012a9f
Angela Lam <30443453+kyrnen@users.noreply.github.com>
2019-06-18 20:18:54 -0700

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  document.getElementById("about").style.visibility = "visible",
+  document.getElementById("projects").style.visibility = "visible",
+  document.getElementById("hobbies").style.visibility = "visible",
+  document.getElementById("resume").style.visibility = "visible";
+}, 3050);
